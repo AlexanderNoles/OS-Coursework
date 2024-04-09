5c3a3316d7077592aedcdf8398bee42b76706bb7
AlexanderNoles <alexnoles@outlook.com>
2024-04-09 17:45:47 +0100

    Added bat file

---
+++ run.bat
@@ -0,0 +1,4 @@
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar Experiment1
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar Experiment2
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar Experiment3
+
